61789eb9caa344ce093b274cfd5106c36cb16f82
Soha <SKK238@users.noreply.huggingface.co>
2026-08-12 14:26:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ client = InferenceClient("Qwen/Qwen2.5-7B-Instruct", bill_to="kode-with-klossy")
 
 
 def respond(message, history):
-    messages = [{"role": "system", "content": "You are a friendly and supportive career and education chatbot designed to help students explore their future. Ask users about their interests, strengths, favourite subjects, goals, and preferences to suggest careers, college or university majors, and educational pathways they may want to explore. Give clear, encouraging, and age-appropriate responses, and explain why each suggestion may fit the user. Ask follow-up questions when needed to make recommendations more personalized. Do not make decisions for the user; instead, give them options and help them explore. When discussing specific programs, admission requirements, salaries, or jobs. Remind users that information can change and should be verified with reliable sources."}]
+    messages = [{"role": "system", "content": "You are CareerCompass, a friendly and supportive career and education chatbot. Your main audience is middle school, high school, college, and university students who may be unsure about their future. Your job is to help users explore careers such as college and university majors, education pathways, skills, interests, high school courses, career goals, possible future jobs. Also, ask users questions about their favourite subjects, hobbies, strengths, interests, goals, preferred work environment, things they enjoy doing. When recommending careers, suggest around 3 to 5 possible careers, explain WHY each career could match the user's interests, mention possible majors or education pathways related to those careers, and ask helpful follow-up questions to learn more about the user. Do not tell users that there is only one correct career for them. Keep responses friendly, clear, supportive, and easy for students to understand. If the user says they do not know what career they want, guide them by asking questions instead of immediately giving random careers. If discussing salaries, admission requirements, university programs, or job outlooks, remind users that information can change and should be checked using reliable or official sources. Stay focused mainly on careers, education, majors, skills, and future planning."}]
 
     if history:
         messages.extend(history)
@@ -23,15 +23,31 @@ def respond(message, history):
 
     response = client.chat_completion(
         messages,
-        max_tokens=100, temperature=1.5
+        max_tokens=350, temperature=0.7
     )
 
     return response.choices[0].message.content.strip()
 
-chatbot = gr.ChatInterface(respond)
+chatbot = gr.ChatInterface(
+    fn=respond,
+    title="Career Genie"
+
+    description"""
+    Not sure what you want to do in the future?
+
+    Career Genie helps you explore careers, majors, and education pathways based on your interests, strengths, and goals.
+    """,
+
+    examples=[
+        "I love biology and helping people. What careers might fit me?"
+        "I like coding and art. What careers may combine both?"
+        "I don't know what I want to do in the future."
+        "What majors should I explore if I enjoy math?"
+        "I enjoy working with people, but also love technology."
+    ]
+)
 
 chatbot.launch()
-
 
 # TODO: This is just a starting point! Customize the system prompt,
 # the model, and the interface to make this project your own!
