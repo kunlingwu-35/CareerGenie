b6de69f212d1f5b5938a0bb217fbcfc283e9f27a
Niral Jagadeesh <Ninae-12@users.noreply.huggingface.co>
2026-08-13 18:22:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -490,7 +490,7 @@ with gr.Blocks(
         <div id="hero">
 
             <img src="file=Career (1).png" 
-                style="width:200px; height:200px; object-fit:contain;">
+                style="width:2000px; height:2000px; object-fit:contain;">
 
             <div style="font-size:60px;">
                 
